7f4d58dd1705bd8374d6ab45cc35aa4a92a1ffb9
Uttam3003 <133484684+Uttam3003@users.noreply.github.com>
2023-08-28 14:32:06 +0530

    Update mint-nft.js

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -1,22 +1,21 @@
-require("dotenv").config();
+require("dotenv").config(); // Load environment variables from .env file
 const API_URL = process.env.API_URL;
 
 const PUBLIC_KEY = process.env.PUBLIC_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
-const web3 = createAlchemyWeb3(API_URL);
+const web3 = createAlchemyWeb3(API_URL); // Create a Web3 instance connected to Alchemy
 
 const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
 
-//console.log(JSON.stringify(contract.abi));
-
 const contractAddress = "0x0429EC2dd779E982ce28eE80786ceEb178A8a92E";
 const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
-//create transaction
-async function mintNFT(tokenURI) {
-  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest"); //get latest nonce
 
-  //the transaction
+// Function to mint an NFT
+async function mintNFT(tokenURI) {
+  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest"); // Get latest nonce
+
+  // Construct the transaction object
   const tx = {
     from: PUBLIC_KEY,
     to: contractAddress,
@@ -28,6 +27,7 @@ async function mintNFT(tokenURI) {
   const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
   signPromise
     .then((signedTx) => {
+      // Send the signed transaction
       web3.eth.sendSignedTransaction(
         signedTx.rawTransaction,
         function (err, hash) {
@@ -50,6 +50,8 @@ async function mintNFT(tokenURI) {
       console.log(" Promise failed:", err);
     });
 }
+
+// Call the mintNFT function with the tokenURI
 mintNFT(
   "https://gateway.pinata.cloud/ipfs/QmQX2tK3AYNPyEiaFxf6fX97QmoTfXxsLhHmaKcqvAdUoq"
 );
