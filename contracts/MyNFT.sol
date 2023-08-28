cd638ae59459c69d82b823056424c9e469d23d42
Uttam3003 <133484684+Uttam3003@users.noreply.github.com>
2023-08-28 14:29:20 +0530

    Update MyNFT.sol

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,28 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
 
+// Import OpenZeppelin contracts
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+// Define the contract
 contract MyNFT is ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
+    using Counters for Counters.Counter; // Import Counters library to manage token IDs
 
-    Counters.Counter private _tokenIds;
+    Counters.Counter private _tokenIds; // Counter to keep track of token IDs
 
+    // Constructor to initialize the ERC721 token
     constructor() ERC721("Code Eater", "CER") {}
 
+    // Function to mint a new NFT
     function mintNFT(address recipient, string memory tokenURI)
         public
-        onlyOwner
+        onlyOwner // Only the owner can mint NFTs
         returns (uint256)
     {
-        _tokenIds.increment();
+        _tokenIds.increment(); // Increment the token ID counter
 
-        uint256 newItemId = _tokenIds.current();
-        _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-        return newItemId;
+        uint256 newItemId = _tokenIds.current(); // Get the current token ID
+        _mint(recipient, newItemId); // Mint the NFT to the recipient's address
+        _setTokenURI(newItemId, tokenURI); // Set the token URI for metadata
+        return newItemId; // Return the new NFT's ID
     }
 }
